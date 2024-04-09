bbd81974876e7e441f52b7f216e7ea0b3b323eff
gun7728 <gun7728@gmail.com>
2024-04-09 15:46:48 +0900

    feat : db 파일 디렉토리 변경 (2024.04.09. 함건)
    feat : mongodb 제거 (2024.04.09. 함건)
    
    feat : sqlite sql 작성 (2024.04.09. 함건)
    
    feat : sqlite 변경 (2024.04.09. 함건)
    
    feat : 전체 적용 (2024.04.09. 함건)

--- main/user.py
+++ main/user.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request, jsonify
-from main import db,now
+from flask import Blueprint, request, jsonify,json
+from main import now
+from main.db.sql import user_sql as user
 import hashlib
 
 blueprint = Blueprint('user', __name__, url_prefix='/user')
@@ -7,13 +8,11 @@ blueprint = Blueprint('user', __name__, url_prefix='/user')
 
 @blueprint.route('/check', methods=['GET'])
 def id_check():
-  user_id = request.args.get('id')
-  print(user_id)
-  result = db.user.find_one({'id': user_id})
-  if result is not None:
-    return jsonify({'result': 'fail', 'msg': '이미 존재 하는 ID 입니다!'})
+  result = user.get(request.args.get('id'))
+  if len(result):
+    return json.dumps({'result': 'fail', 'msg': '이미 존재 하는 ID 입니다!'}, ensure_ascii=False)
   else:
-    return jsonify({'result': 'success', 'msg': '생성 가능한 ID 입니다.'})
+    return json.dumps({'result': 'success', 'msg': '생성 가능한 ID 입니다.'}, ensure_ascii=False)
 
 
 @blueprint.route('/register',methods=['POST'])
@@ -25,45 +24,46 @@ def register():
   user_pw_hash = hashlib.sha256(user_pw.encode('utf-8')).hexdigest()
 
   # 이미 존재하는 아이디면 패스!
-  result = db.user.find_one({'id': user_id})
-  if result is not None:
+  result = user.get(user_id)
+  if len(result):
     return jsonify({'result': 'fail', 'msg': '이미 존재 하는 ID 입니다!'})
   else:
-    db.user.insert_one({'id': user_id, 'pw': user_pw_hash, 'email':user_email, 'nick': user_nickname, 'created_at': now})
+    user.insert_one(user_id, user_nickname, user_pw_hash, user_email, now)
   return jsonify({'result': 'success'})
 
 
 
 @blueprint.route('/get')
 def get_all_user():
-  all_data = list(db.user.find({},{'_id':False}))
+  all_data = user.get_all()
+  print(all_data)
   return all_data
-
-@blueprint.route('/login', methods=['POST'])
-def login():
-  user_id = request.form['id']
-  user_pw = request.form['pw']
-
-  # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
-  user_pw_hash = hashlib.sha256(user_pw.encode('utf-8')).hexdigest()
-
-  # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
-  result = db.user.find_one({'id': user_id, 'pw': user_pw_hash})
-  print(user_pw_hash)
-
-  # 찾으면 JWT 토큰을 만들어 발급합니다.
-  if result is not None:
-    # # JWT 토큰 생성
-    # payload = {
-    #   'id': id_receive,
-    #   'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
-    # }
-    # token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
-    #
-    # # token을 줍니다.
-    return jsonify({'result': 'success', 'id':user_id})
-  else:
-    return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+#
+# @blueprint.route('/login', methods=['POST'])
+# def login():
+#   user_id = request.form['id']
+#   user_pw = request.form['pw']
+#
+#   # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
+#   user_pw_hash = hashlib.sha256(user_pw.encode('utf-8')).hexdigest()
+#
+#   # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
+#   result = db.user.find_one({'id': user_id, 'pw': user_pw_hash})
+#   print(user_pw_hash)
+#
+#   # 찾으면 JWT 토큰을 만들어 발급합니다.
+#   if result is not None:
+#     # # JWT 토큰 생성
+#     # payload = {
+#     #   'id': id_receive,
+#     #   'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
+#     # }
+#     # token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+#     #
+#     # # token을 줍니다.
+#     return jsonify({'result': 'success', 'id':user_id})
+#   else:
+#     return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 @blueprint.route('/edit')
 def edit():
